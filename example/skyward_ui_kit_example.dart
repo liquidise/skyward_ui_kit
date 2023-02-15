56bf6709aefb06e37e32e85fc2e497ae15b527dc
Ben Roux <liquid.ise@gmail.com>
2023-02-14 20:07:26 -0700

    Initial commit:

---
+++ example/skyward_ui_kit_example.dart
@@ -0,0 +1,6 @@
+import 'package:skyward_ui_kit/skyward_ui_kit.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/skyward_ui_kit_test.dart
@@ -0,0 +1,16 @@
+import 'package:skyward_ui_kit/skyward_ui_kit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
